65c7331a7238f6807d8f1f2b9a75c4a0331e1bd1
Daniel Nempeque <daniel.nempeque@gmail.com>
2021-04-25 16:31:39 -0500

    Adding deploy service with Azure container registry

--- deploy_service.groovy
+++ deploy_service.groovy
@@ -1,7 +1,5 @@
 pipeline {
-    agent {
-        docker { image 'mcr.microsoft.com/azure-cli:latest'}
-    }
+    agent any
     environment {
         ACR_REGISTRY = 'cicdworkshop.azurecr.io'
         ACR_RES_GROUP = 'ci-cd-workshop'
@@ -15,9 +13,11 @@ pipeline {
                     usernamePassword(credentialsId: 'AzureACR', usernameVariable:'ACR_USER', passwordVariable: 'ACR_PASSWORD'),
                     azureServicePrincipal('AzureServicePrincipal')
                 ]) {
-                    sh "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID"
-                    sh "az webapp config container set --docker-custom-image-name $Image_name --docker-registry-server-password $ACR_PASSWORD --docker-registry-server-url https://$ACR_REGISTRY --docker-registry-server-user $ACR_USER --name emojiselector-dev --resource-group $ACR_RES_GROUP"
-                    sh "az webapp restart --name emojiselector-dev --resource-group $ACR_RES_GROUP"
+                    docker.image('mcr.microsoft.com/azure-cli:latest').inside(){
+                        sh "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID"
+                        sh "az webapp config container set --docker-custom-image-name $Image_name --docker-registry-server-password $ACR_PASSWORD --docker-registry-server-url https://$ACR_REGISTRY --docker-registry-server-user $ACR_USER --name emojiselector-dev --resource-group $ACR_RES_GROUP"
+                        sh "az webapp restart --name emojiselector-dev --resource-group $ACR_RES_GROUP"
+                    }
                 }
             }
         }
