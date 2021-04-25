5ce2033ff7d75d39043260ce54b83658be454d23
Daniel Nempeque <daniel.nempeque@gmail.com>
2021-04-25 16:40:22 -0500

    Adding deploy service with Azure container registry

--- deploy_service.groovy
+++ deploy_service.groovy
@@ -13,7 +13,8 @@ pipeline {
                     usernamePassword(credentialsId: 'AzureACR', usernameVariable:'ACR_USER', passwordVariable: 'ACR_PASSWORD'),
                     azureServicePrincipal('AzureServicePrincipal')
                 ]) {
-                    sh "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID"
+                    sh "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID "
+                    sh "az account set --subscription $AZURE_SUBSCRIPTION_ID"
                     sh "az webapp config container set --docker-custom-image-name $Image_name --docker-registry-server-password $ACR_PASSWORD --docker-registry-server-url https://$ACR_REGISTRY --docker-registry-server-user $ACR_USER --name emojiselector-dev --resource-group $ACR_RES_GROUP"
                     sh "az webapp restart --name emojiselector-dev --resource-group $ACR_RES_GROUP"
                 }
