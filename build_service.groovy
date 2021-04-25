8f3156ec3e457754016178568d3afbe7afea6f35
Daniel Nempeque <daniel.nempeque@gmail.com>
2021-04-25 12:09:12 -0500

    Adding build service with Azure container registry

--- build_service.groovy
+++ build_service.groovy
@@ -16,18 +16,15 @@ pipeline {
             }
         }
         stage('Build artifact'){
-            steps{
-                step{
-                    def date= new Date()
-                    def image_name = "$env.BRANCH_NAME" + date.format("yyMMdd", TimeZone.getTimeZone('CST'))
-                }
-                acrQuickBuild azureCredentialsId: 'AzureServicePrincipal',
-                    resourceGroupName: env.ACR_RES_GROUP,
-                    registryName: env.ACR_NAME,
-                    platform: "Linux",
-                    dockerfile: "Dockerfile",
-                    imageNames: [[image: "$env.ACR_REGISTRY/$image_name:$env.BUILD_NUMBER"]]
-            }
+            def date= new Date()
+            def image_name = "$env.BRANCH_NAME" + date.format("yyMMdd", TimeZone.getTimeZone('CST'))
+            
+            acrQuickBuild azureCredentialsId: 'AzureServicePrincipal',
+                resourceGroupName: env.ACR_RES_GROUP,
+                registryName: env.ACR_NAME,
+                platform: "Linux",
+                dockerfile: "Dockerfile",
+                imageNames: [[image: "$env.ACR_REGISTRY/$image_name:$env.BUILD_NUMBER"]]
         }
     }
 }
