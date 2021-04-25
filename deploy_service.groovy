dfc1feefddc7e0749ee12943d5d46bc65f9be2ee
Daniel Nempeque <daniel.nempeque@gmail.com>
2021-04-25 16:13:51 -0500

    Adding deploy service with Azure container registry

--- deploy_service.groovy
+++ deploy_service.groovy
@@ -1,14 +1,18 @@
 pipeline {
     agent any
+    environment {
+        ACR_REGISTRY = 'cicdworkshop.azurecr.io'
+        ACR_RES_GROUP = 'ci-cd-workshop'
+        ACR_NAME = 'cicdworkshop'
+    }
     stages {
-        stage('Pull image'){
-            steps{
-                echo 'pull image'
-            }
-        }
         stage('Deploy service DEV'){
             steps{
                 echo 'Deploying service'
+                withCredentials([usernamePassword(credentialsId: 'AzureACR', usernameVariable:'ACR_USER', passwordVariable: 'ACR_PASSWORD')]) {
+                    sh "az webapp config container set --docker-custom-image-name $Image_name --docker-registry-server-password $ACR_PASSWORD --docker-registry-server-url https://$ACR_REGISTRY --docker-registry-server-user $ACR_USER --name emojiselector-dev --resource-group $ACR_RES_GROUP"
+                    sh "az webapp restart --name emojiselector-dev --resource-group $ACR_RES_GROUP"
+                }
             }
         }
         stage('Deploy service STG'){
