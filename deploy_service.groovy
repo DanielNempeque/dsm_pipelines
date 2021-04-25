1f98aa122489b7a5c203613c6c80dbb12c754283
Daniel Nempeque <daniel.nempeque@gmail.com>
2021-04-25 17:22:50 -0500

    Adding deploy service with Azure container registry

--- deploy_service.groovy
+++ deploy_service.groovy
@@ -31,7 +31,9 @@ pipeline {
                 expression { return "$Environments".contains('Prod') }
             }
             steps{
-                def userInput = input message: 'Approve plan output for production?', ok: 'Approve plan'
+                script{
+                    def userInput = input message: 'Approve plan output for production?', ok: 'Approve plan'
+                }
                 echo 'Deploying service'
                 withCredentials([
                     usernamePassword(credentialsId: 'AzureACR', usernameVariable:'ACR_USER', passwordVariable: 'ACR_PASSWORD'),
